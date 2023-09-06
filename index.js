ddccb1fb9baa0f417dd0bb086ddd0d1f5bba2d92
merrickbeauchamp <109994551+merrickbeauchamp@users.noreply.github.com>
2023-09-05 21:09:32 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser')
+const multer = require('multer') 
+const upload = multer() 
+
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
+
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: true })) 
+
+app.get('/', function(req, res) {
+  res.send({success: true})
+})
+
+async function database(id) {
+  let blockedList = await db.get("blocked")
+  let blocked = false;
+  if(blockedList.includes(id)) blocked = true
+  let date = new Date().getDate()
+  let todayData = await db.get("library_" + date)
+  if(!todayData) {
+    todayData = await db.set("library_" + date, [])
+    if(await db.get("library_" + (date - 1))) {
+      await db.delete("library_" + (date - 1))
+    }
+  }
+  let entry = true
+  let students = todayData.length
+  let max = await db.get("max")
+  max = max ? max : 60
+  let full = false
+  if(max <= students) full = true
+  if(todayData.includes(id)) {
+    full = false
+    await db.pull("library_" + date, id)
+    entry = false
+    students--
+  } else {
+    if(!full) {
+      await db.push("library_" + date, id)
+    students++
+    }
+  }
+  
+  let currentStudents = db.get("library_" + date)
+  return {success: true, blocked, entry, id, full}
+}
+
+app.post('/scan', async function(req, res) {
+  if(!req.body?.id) return res.send({success: false})
+  const response = await database(req.body.id)
+  res.send(response)
+})
+
+app.post('/block', async function(req, res) {
+  if(!req.body?.id) return res.send({success: false})
+  let blocked = await db.get("blocked")
+  if(!blocked) {
+     await db.push("blocked", req.body.id)
+    return res.send({success: true, blocked: true})
+  }
+  if(blocked.includes(req.body.id)) {
+    await db.pull("blocked", req.body.id)
+    return res.send({success: true, blocked: false})
+  } else {
+    await db.push("blocked", req.body.id)
+    return res.send({success: true, blocked: true})
+  }
+  
+})
+
+app.post('/changemax', async function(req, res) {
+  if(!req.body?.max) return res.send({success: false})
+  await db.set("max", req.body.max)
+  return res.send({success: true, max:req.body.max})
+})
+
+app.get('/students', async function(req, res) {
+   let date = new Date().getDate()
+  let students = await db.get("library_" + date)
+  if(!students) students = []
+  let max = await db.get("max")
+  if(!max) max = 60
+  return res.send({success: true, students, max})
+})
+
+app.listen()
