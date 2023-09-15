64c5b44ddd3f3291a8136bc856283a34e2a40059
merrickbeauchamp <109994551+merrickbeauchamp@users.noreply.github.com>
2023-09-14 20:03:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 const bodyParser = require('body-parser')
 const multer = require('multer') 
 const upload = multer() 
-const path = require('path')
+//const path = require('path')
 
 const { Database } = require("quickmongo") ;
 
@@ -24,13 +24,13 @@ load()
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true })) 
 
-const html = path.join(__dirname, 'library');
+//const html = path.join(__dirname, 'library');
 
-app.use('/', express.static(html));
+//app.use('/', express.static(html));
 
-app.get('/librarian', function(req, res) {
+/*app.get('/librarian', function(req, res) {
     res.sendFile(path.join(__dirname, '/master.html'));
-})
+})*/
 
 app.get('/ping', function(req, res) {
   res.send({success: true})
